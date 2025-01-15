e8e3d6fb289443a87bfbf1dd3fa833faad239073
Douglas Matoso <doug2k1@gmail.com>
2025-01-15 20:37:09 -0300

    Fix docs build

--- site.config.js
+++ site.config.js
@@ -1,9 +1,11 @@
-module.exports = {
+export default {
   build: {
-    srcPath: './src',
-    outputPath: './public'
+    srcPath: './docs-src',
+    outputPath: './docs',
   },
   site: {
-    title: 'Nanogen site'
-  }
-};
+    title: 'Nanogen',
+    description: 'Minimalist static site generator in Node.js',
+    basePath: process.env.NODE_ENV === 'production' ? '/nanogen' : '',
+  },
+}
